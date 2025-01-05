259e212610ee0b202db61bfc12fe46c21ba42d95
aa-r-ju <aarju3447@gmail.com>
2025-01-05 17:15:52 +0545

    feat: completed object-properties of TDD

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -1,1 +1,27 @@
 /* eslint-disable no-unused-vars */
+function setPropsOnObj(obj) {
+  obj.x = 7;
+  obj["y"] = 8;
+  obj.onePlus = (num) => num + 1;
+}
+
+function setPropsOnArr(arr) {
+  arr.hello = () => "Hello!";
+  arr.full = "stack";
+  arr[0] = 5;
+  arr.twoTimes = (num) => num * 2;
+}
+
+function setPropsOnFunc(func) {
+  func.year = "20??";
+  func.divideByTwo = (num) => num / 2;
+}
+
+function shallowCopy(item1, item2) {
+  if (Array.isArray(item1) && Array.isArray(item2)) {
+    return [...item1, ...item2];
+  } else if (typeof item1 === "object" && typeof item2 === "object") {
+    return { ...item1, ...item2 };
+  }
+  return null;
+}
--- tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
+++ tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+let dog = {
+  bark() {
+    return "ruff ruff!";
+  },
+
+  name: "Cody",
+
+  loudBark() {
+    return this.bark().toUpperCase();
+  },
+};
