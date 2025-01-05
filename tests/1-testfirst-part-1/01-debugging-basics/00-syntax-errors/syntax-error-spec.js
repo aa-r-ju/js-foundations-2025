46dc1acb301f1d1e2b1dd2f0cb62089acb35a446
aa-r-ju <aarju3447@gmail.com>
2025-01-05 16:46:00 +0545

    feat: completed syntax-error of TDD

--- tests/1-testfirst-part-1/01-debugging-basics/00-syntax-errors/syntax-error-spec.js
+++ tests/1-testfirst-part-1/01-debugging-basics/00-syntax-errors/syntax-error-spec.js
@@ -1,21 +1,21 @@
 /* eslint-env jasmine */
 /* eslint-disable no-undef */
 
-describe('helloWorld', () => {
-  it('a `hi` variable is in the global scope', () => {
-    expect(hi).toBe('hi');
+describe("helloWorld", () => {
+  it("a `hi` variable is in the global scope", () => {
+    expect(hi).toBe("hi");
   });
 
-  it('a `world` variable is in the global scope', () => {
-    expect(world).toBe('world');
+  it("a `world` variable is in the global scope", () => {
+    expect(world).toBe("world");
   });
 
-  it('the function helloWorld returns a string', () => {
+  it("the function helloWorld returns a string", () => {
     expect(helloWorld).toBeDefined();
-    expect(helloWorld()).toBe('hi world');
+    expect(helloWorld()).toBe("hi world");
   });
 
-  it('the object `goodbyeWorld` has three keys', () => {
+  it("the object `goodbyeWorld` has three keys", () => {
     const arrayOfKeys = Object.keys(goodbyeWorld);
     expect(arrayOfKeys.length).toBe(3);
   });
