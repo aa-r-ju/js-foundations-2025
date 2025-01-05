b7dfe090a7b0e550bb43c208b7060f8c176828d9
aa-r-ju <aarju3447@gmail.com>
2025-01-05 16:33:31 +0545

    feat: done reading of TDD

--- tests/1-testfirst-part-1/00-reading-test-specs/reading.js
+++ tests/1-testfirst-part-1/00-reading-test-specs/reading.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 function hello(name) {
-  if (typeof name === 'undefined') {
-    return 'Hello!';
+  if (typeof name === "undefined") {
+    return "Hello!";
   } else {
     return `Hello, ${name}!`;
   }
@@ -10,3 +10,8 @@ function hello(name) {
 function add(number, num) {
   return number + num;
 }
+
+/* eslint-env jasmine */
+/* eslint-disable no-undef */
+let FILL_ME_IN = "Fred";
+console.log(hello(FILL_ME_IN));
