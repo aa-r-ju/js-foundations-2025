5c1ab4775cc8c3ef8777364ad7d4de54a4c168e5
aa-r-ju <aarju3447@gmail.com>
2025-01-06 18:34:11 +0545

    fix: object-syntax

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -1,27 +1,50 @@
 /* eslint-disable no-unused-vars */
+// function setPropsOnObj(obj) {
+//   obj.x = 7;
+//   obj["y"] = 8;
+//   obj.onePlus = (num) => num + 1;
+// }
+
+// function setPropsOnArr(arr) {
+//   arr.hello = () => "Hello!";
+//   arr.full = "stack";
+//   arr[0] = 5;
+//   arr.twoTimes = (num) => num * 2;
+// }
+
+// function setPropsOnFunc(func) {
+//   func.year = "20??";
+//   func.divideByTwo = (num) => num / 2;
+// }
+
+// function shallowCopy(item1, item2) {
+//   if (Array.isArray(item1) && Array.isArray(item2)) {
+//     return [...item1, ...item2];
+//   } else if (typeof item1 === "object" && typeof item2 === "object") {
+//     return { ...item1, ...item2 };
+//   }
+//   return null;
+// }
+
 function setPropsOnObj(obj) {
   obj.x = 7;
   obj["y"] = 8;
   obj.onePlus = (num) => num + 1;
 }
-
 function setPropsOnArr(arr) {
   arr.hello = () => "Hello!";
   arr.full = "stack";
   arr[0] = 5;
   arr.twoTimes = (num) => num * 2;
 }
-
 function setPropsOnFunc(func) {
   func.year = "20??";
   func.divideByTwo = (num) => num / 2;
 }
-
-function shallowCopy(item1, item2) {
-  if (Array.isArray(item1) && Array.isArray(item2)) {
-    return [...item1, ...item2];
-  } else if (typeof item1 === "object" && typeof item2 === "object") {
-    return { ...item1, ...item2 };
+function shallowCopy(obj1, obj2) {
+  if (Array.isArray(obj1) && Array.isArray(obj2)) {
+    return [...obj1, ...obj2];
+  } else {
+    return { ...obj1, ...obj2 };
   }
-  return null;
 }
--- tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
+++ tests/1-testfirst-part-1/02-object-properties/object-syntax-practice/object-syntax.js
@@ -1,13 +1,20 @@
 /* eslint-disable no-unused-vars */
 
+let loud = "loud";
+
+let name = "Cody";
+
 let dog = {
+  name,
+
   bark() {
     return "ruff ruff!";
   },
 
-  name: "Cody",
-
   loudBark() {
-    return this.bark().toUpperCase();
+    if (loud === "loud") {
+      return this.bark().toUpperCase();
+    }
+    return this.bark();
   },
 };
