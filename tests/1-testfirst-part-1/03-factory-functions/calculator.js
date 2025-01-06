b21b91ba12881fd02e424e826bc02e64f927a75b
aa-r-ju <aarju3447@gmail.com>
2025-01-06 17:14:44 +0545

    feat: completed calculator of TDD

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,49 @@
 /* eslint-disable no-unused-vars */
+function createCalculator() {
+  let total = 0;
+
+  return {
+    add(num) {
+      total += num;
+    },
+    subtract(num) {
+      total -= num;
+    },
+    value() {
+      return total;
+    },
+    clear() {
+      total = 0;
+    },
+  };
+}
+
+function addSquareMethod(calculators) {
+  return calculators.map((calculator) => {
+    calculator.square = function () {
+      return this.value() ** 2;
+    };
+    return calculator;
+  });
+}
+
+const humanCalculatorMethods = {
+  add(num) {
+    this.total += num;
+  },
+  subtract(num) {
+    this.total -= num;
+  },
+  value() {
+    return this.total;
+  },
+  clear() {
+    this.total = -10;
+  },
+};
+
+function createHumanCalculator() {
+  const instance = Object.create(humanCalculatorMethods);
+  instance.total = -10;
+  return instance;
+}
