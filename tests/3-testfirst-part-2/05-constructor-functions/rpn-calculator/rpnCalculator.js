25259874bc4b8239067373d820e75c58ede5a2d4
aa-r-ju <aarju3447@gmail.com>
2025-01-08 21:10:00 +0545

    feat;completed rpncalculator

--- tests/3-testfirst-part-2/05-constructor-functions/rpn-calculator/rpnCalculator.js
+++ tests/3-testfirst-part-2/05-constructor-functions/rpn-calculator/rpnCalculator.js
@@ -1,1 +1,59 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(value) {
+    this.stack.push(value);
+  }
+
+  value() {
+    if (this.stack.length === 0) throw "rpnCalculatorInstance is empty";
+    return this.stack[this.stack.length - 1];
+  }
+
+  plus() {
+    this.checkStack(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a + b);
+  }
+
+  minus() {
+    this.checkStack(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a - b);
+  }
+
+  times() {
+    this.checkStack(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a * b);
+  }
+
+  divide() {
+    this.checkStack(2);
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    if (b === 0) throw "Division by zero";
+    this.stack.push(a / b);
+  }
+
+  checkStack(required) {
+    if (this.stack.length < required) {
+      throw "rpnCalculatorInstance is empty";
+    }
+  }
+}
+
+const calculator = new RPNCalculator();
+
+// Push values onto the stack
+calculator.push(5);
+calculator.push(3);
+// calculator.plus(); // Adds 5 + 3
+console.log("Result after addition:", calculator.value()); // Outputs: 8
+console.log(calculator);
