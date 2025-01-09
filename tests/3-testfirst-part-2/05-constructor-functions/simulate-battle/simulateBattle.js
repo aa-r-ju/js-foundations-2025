1d5867c294bc4a29649c28430a53de9ad62685db
aa-r-ju <aarju3447@gmail.com>
2025-01-09 22:13:48 +0545

    feat:completed simulatebattle

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,1 +1,40 @@
 /* eslint-disable no-unused-vars, no-throw-literal*/
+function Pokemon(name, health, attackBonus) {
+  this.name = name;
+  this.health = health;
+  this.attackBonus = attackBonus;
+}
+
+Pokemon.prototype.biteAttack = function () {
+  return this.attackBonus + 2;
+};
+
+Pokemon.prototype.isDefeated = function () {
+  return this.health <= 0;
+};
+
+function simulateBattle(pokemon1, pokemon2, firstAttackerName) {
+  let attacker = pokemon1.name === firstAttackerName ? pokemon1 : pokemon2;
+  let defender = pokemon1.name === firstAttackerName ? pokemon2 : pokemon1;
+
+  while (true) {
+    defender.health -= attacker.biteAttack();
+
+    if (defender.isDefeated()) {
+      return `${attacker.name} Wins!`;
+    }
+
+    [attacker, defender] = [defender, attacker];
+  }
+}
+
+const krabby = new Pokemon("Krabby", 100, 50);
+const squirtle = new Pokemon("Squirtle", 50, 0);
+
+console.log(simulateBattle(krabby, squirtle, "Krabby"));
+
+const pikachu = new Pokemon("Pikachu", 250, 4);
+const charizard = new Pokemon("Charizard", 300, 5);
+
+console.log(simulateBattle(pikachu, charizard, "Pikachu"));
+console.log(simulateBattle(pikachu, charizard, "Charizard"));
