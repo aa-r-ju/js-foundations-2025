374d5b5b7b1c3a1a73ec3d9bc0cffe964472d122
aa-r-ju <aarju3447@gmail.com>
2025-01-10 19:27:33 +0545

    feat: completed function

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,1 +1,59 @@
 /* eslint-disable no-unused-vars */
+// Function for `concatString`
+function concatString() {
+  return Array.from(arguments).join("");
+}
+
+function yourFunctionRunner() {
+  return Array.from(arguments)
+    .map((fn) => fn())
+    .join("");
+}
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+function once(func) {
+  let called = false;
+  let result;
+  return function () {
+    if (!called) {
+      called = true;
+      result = func();
+      return result;
+    }
+    return "the function has already been called...";
+  };
+}
+
+function createObjectWithClosures() {
+  let value = 0;
+  return {
+    oneIncrementer: function () {
+      value += 1;
+    },
+    tensIncrementer: function () {
+      value += 10;
+    },
+    getValue: function () {
+      return value;
+    },
+    setValue: function (newValue) {
+      value = newValue;
+    },
+  };
+}
+
+function dontSpillTheBeans(secret) {
+  return {
+    getSecret: function () {
+      return secret;
+    },
+    setSecret: function (newSecret) {
+      secret = newSecret;
+    },
+  };
+}
