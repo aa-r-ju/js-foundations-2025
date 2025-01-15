2eb2c1fb9b29a397e5f624206be22b8930329392
aa-r-ju <aarju3447@gmail.com>
2025-01-15 16:05:41 +0545

    feat: completed macbooks

--- tests/6-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
+++ tests/6-testfirst-part-3/08-Classes/02_macbooks/macbooks.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-unused-vars */
+// Laptop class definition
+class Laptop {
+  constructor(year, hd) {
+    this.year = year;
+    this.hd = hd;
+  }
+
+  // Prototype method to check specifications
+  checkSpecs() {
+    return `Year: ${this.year}, HD: ${this.hd}`;
+  }
+}
+
+// Macbook class definition, extends Laptop
+class Macbook extends Laptop {
+  constructor(year, hd, color) {
+    super(year, hd); // Call the parent class constructor
+    this.color = color;
+  }
+}
