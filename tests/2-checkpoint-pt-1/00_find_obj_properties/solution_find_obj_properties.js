b04eba81c3203597a7b4adf27b4ceffe6707f4a7
aa-r-ju <aarju3447@gmail.com>
2025-01-16 23:31:32 +0545

    feat: completed solution-find-obj-properties

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,40 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+// Part 1: Using `hasOwnProperty`
+function findObjPropsHasOwn(obj) {
+  const keys = [];
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      keys.push(key);
+    }
+  }
+  return keys.join(", ");
+}
+
+// Part 2: Using `Object.keys`
+function findObjKeys(obj) {
+  return Object.keys(obj).join(", ");
+}
+
+const rectanglePrototype = {
+  getArea: function () {
+    return this.height * this.width;
+  },
+};
+
+// function rectangle(color, height, width) {
+//   const rectangleInstance = Object.create(rectanglePrototype);
+
+//   rectangleInstance.color = color;
+//   rectangleInstance.height = height;
+//   rectangleInstance.width = width;
+
+//   return rectangleInstance;
+// }
+
+// // Test case 1: findObjPropsHasOwn
+// const blueRectangle = rectangle("blue", 5, 3);
+// console.log(findObjPropsHasOwn(blueRectangle)); // Output: "color, height, width"
+
+// // Test case 2: findObjKeys
+// const purpleRectangle = rectangle("purple", 7, 2);
+// console.log(findObjKeys(purpleRectangle)); // Output: "color, height, width"
