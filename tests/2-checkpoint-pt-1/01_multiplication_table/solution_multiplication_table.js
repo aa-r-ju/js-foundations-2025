58f0cae0352837f2220e9c608a70e45aea8365e3
aa-r-ju <aarju3447@gmail.com>
2025-01-17 20:13:54 +0545

    feat: completed solution-multiplication-table

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,14 @@
 /* eslint-disable no-unused-vars */
+function multiplicationTable(rows, columns) {
+  const table = [];
+
+  for (let i = 1; i <= rows; i++) {
+    const row = [];
+    for (let j = 1; j <= columns; j++) {
+      row.push(i * j);
+    }
+    table.push(row);
+  }
+
+  return table;
+}
