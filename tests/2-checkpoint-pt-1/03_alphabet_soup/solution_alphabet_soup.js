5db55ab3c67dc5b24dfc6904888e700a0005655c
aa-r-ju <aarju3447@gmail.com>
2025-01-17 20:20:45 +0545

    feat: completed solution-alphabet-soup

--- tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
+++ tests/2-checkpoint-pt-1/03_alphabet_soup/solution_alphabet_soup.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function soup(phrase, characterBank) {
+  const bankCount = {};
+
+  // Count occurrences of each character in the character bank
+  for (const char of characterBank) {
+    bankCount[char] = (bankCount[char] || 0) + 1;
+  }
+
+  // Check if there are enough characters in the bank for the phrase
+  for (const char of phrase) {
+    if (!bankCount[char] || bankCount[char] <= 0) {
+      return false; // Not enough characters
+    }
+    bankCount[char]--; // Use one character from the bank
+  }
+
+  return true; // All characters in the phrase are covered
+}
