87704411e3f248d483e31e1ba8140e284136f126
aa-r-ju <aarju3447@gmail.com>
2025-01-22 21:18:04 +0545

    code revising

--- tests/1-testfirst-part-1/01-debugging-basics/01-reference-error/reference-error.js
+++ tests/1-testfirst-part-1/01-debugging-basics/01-reference-error/reference-error.js
@@ -1,10 +1,19 @@
-const france = "france";
-const paris = "paris";
-const marsielle = france;
+// const france = "france";
+// const paris = "paris";
+// const marsielle = france;
 
-const vacationSpots = (spot1, spot2) => {
-  if (spot1.length > spot2.length) {
-    return spot1;
+// const vacationSpots = (spot1, spot2) => {
+//   if (spot1.length > spot2.length) {
+//     return spot1;
+//   }
+//   return spot2;
+// };
+
+//practicing code
+let france = "france";
+function vacationSpots(val1, val2) {
+  if (val1.length > val2.length) {
+    return val1;
   }
-  return spot2;
-};
+  return val2;
+}
