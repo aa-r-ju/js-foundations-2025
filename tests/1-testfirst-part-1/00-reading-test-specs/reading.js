49bec7a56d9a7352f62b2b08019202c855f26cff
aa-r-ju <aarju3447@gmail.com>
2025-01-22 19:26:37 +0545

    practicing code

--- tests/1-testfirst-part-1/00-reading-test-specs/reading.js
+++ tests/1-testfirst-part-1/00-reading-test-specs/reading.js
@@ -1,17 +1,28 @@
 /* eslint-disable no-unused-vars */
+// function hello(name) {
+//   if (typeof name === "undefined") {
+//     return "Hello!";
+//   } else {
+//     return `Hello, ${name}!`;
+//   }
+// }
+
+// function add(number, num) {
+//   return number + num;
+// }
+
+// /* eslint-env jasmine */
+// /* eslint-disable no-undef */
+// let FILL_ME_IN = "Fred";
+// console.log(hello(FILL_ME_IN));
+
 function hello(name) {
-  if (typeof name === "undefined") {
-    return "Hello!";
-  } else {
-    return `Hello, ${name}!`;
+  if (name === undefined) {
+    return `Hello!`;
   }
+  return `Hello, ${name}!`;
 }
 
-function add(number, num) {
-  return number + num;
+function add(num1, num2) {
+  return num1 + num2;
 }
-
-/* eslint-env jasmine */
-/* eslint-disable no-undef */
-let FILL_ME_IN = "Fred";
-console.log(hello(FILL_ME_IN));
