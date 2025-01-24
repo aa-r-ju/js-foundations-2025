9418d12bb3286bf6020aa8f3c61e01a5cde0ea33
aa-r-ju <aarju3447@gmail.com>
2025-01-24 19:43:44 +0545

    practicing code

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -26,25 +26,53 @@
 //   return null;
 // }
 
+// function setPropsOnObj(obj) {
+//   obj.x = 7;
+//   obj["y"] = 8;
+//   obj.onePlus = (num) => num + 1;
+// }
+// function setPropsOnArr(arr) {
+//   arr.hello = () => "Hello!";
+//   arr.full = "stack";
+//   arr[0] = 5;
+//   arr.twoTimes = (num) => num * 2;
+// }
+// function setPropsOnFunc(func) {
+//   func.year = "20??";
+//   func.divideByTwo = (num) => num / 2;
+// }
+// function shallowCopy(obj1, obj2) {
+//   if (Array.isArray(obj1) && Array.isArray(obj2)) {
+//     return [...obj1, ...obj2];
+//   } else {
+//     return { ...obj1, ...obj2 };
+//   }
+// }
+
 function setPropsOnObj(obj) {
   obj.x = 7;
   obj["y"] = 8;
   obj.onePlus = (num) => num + 1;
 }
+
 function setPropsOnArr(arr) {
   arr.hello = () => "Hello!";
-  arr.full = "stack";
+  arr["full"] = "stack";
   arr[0] = 5;
-  arr.twoTimes = (num) => num * 2;
+  arr.twoTimes = (val) => val * 2;
 }
+
 function setPropsOnFunc(func) {
   func.year = "20??";
   func.divideByTwo = (num) => num / 2;
 }
-function shallowCopy(obj1, obj2) {
-  if (Array.isArray(obj1) && Array.isArray(obj2)) {
-    return [...obj1, ...obj2];
+
+function shallowCopy(item1, item2) {
+  if (Array.isArray(item1) && Array.isArray(item2)) {
+    return [...item1, ...item2];
+  } else if (typeof item1 === "object" && typeof item2 === "object") {
+    return { ...item1, ...item2 };
   } else {
-    return { ...obj1, ...obj2 };
+    return null;
   }
 }
