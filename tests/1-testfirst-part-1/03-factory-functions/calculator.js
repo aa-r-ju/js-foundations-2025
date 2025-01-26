25421910db6e5629c336abd64e73288b093f84dd
aa-r-ju <aarju3447@gmail.com>
2025-01-26 14:39:12 +0545

    practicing the code

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,4 +1,53 @@
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
+// function createCalculator() {
+//   let total = 0;
+
+//   return {
+//     add(num) {
+//       total += num;
+//     },
+//     subtract(num) {
+//       total -= num;
+//     },
+//     value() {
+//       return total;
+//     },
+//     clear() {
+//       total = 0;
+//     },
+//   };
+// }
+
+// function addSquareMethod(calculators) {
+//   return calculators.map((calculator) => {
+//     calculator.square = function () {
+//       return this.value() ** 2;
+//     };
+//     return calculator;
+//   });
+// }
+
+// const humanCalculatorMethods = {
+//   add(num) {
+//     this.total += num;
+//   },
+//   subtract(num) {
+//     this.total -= num;
+//   },
+//   value() {
+//     return this.total;
+//   },
+//   clear() {
+//     this.total = -10;
+//   },
+// };
+
+// function createHumanCalculator() {
+//   const instance = Object.create(humanCalculatorMethods);
+//   instance.total = -10;
+//   return instance;
+// }
+
 function createCalculator() {
   let total = 0;
 
@@ -16,14 +65,15 @@ function createCalculator() {
       total = 0;
     },
   };
+  return total;
 }
 
-function addSquareMethod(calculators) {
-  return calculators.map((calculator) => {
-    calculator.square = function () {
+function addSquareMethod(instances) {
+  return instances.map((instance) => {
+    instance.square = function () {
       return this.value() ** 2;
     };
-    return calculator;
+    return instance;
   });
 }
 
@@ -41,7 +91,6 @@ const humanCalculatorMethods = {
     this.total = -10;
   },
 };
-
 function createHumanCalculator() {
   const instance = Object.create(humanCalculatorMethods);
   instance.total = -10;
