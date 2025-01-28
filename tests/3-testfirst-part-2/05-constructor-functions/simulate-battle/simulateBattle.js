77ace177e22457d36ac2254d4cf968493dd01892
aa-r-ju <aarju3447@gmail.com>
2025-01-28 19:16:29 +0545

    practicing the code

--- tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
+++ tests/3-testfirst-part-2/05-constructor-functions/simulate-battle/simulateBattle.js
@@ -1,10 +1,52 @@
-/* eslint-disable no-unused-vars, no-throw-literal*/
+// /* eslint-disable no-unused-vars, no-throw-literal*/
+// function Pokemon(name, health, attackBonus) {
+//   this.name = name;
+//   this.health = health;
+//   this.attackBonus = attackBonus;
+// }
+
+// Pokemon.prototype.biteAttack = function () {
+//   return this.attackBonus + 2;
+// };
+
+// Pokemon.prototype.isDefeated = function () {
+//   return this.health <= 0;
+// };
+
+// function simulateBattle(pokemon1, pokemon2, firstAttackerName) {
+//   let attacker = pokemon1.name === firstAttackerName ? pokemon1 : pokemon2;
+//   let defender = pokemon1.name === firstAttackerName ? pokemon2 : pokemon1;
+
+//   while (true) {
+//     defender.health -= attacker.biteAttack();
+
+//     if (defender.isDefeated()) {
+//       return `${attacker.name} Wins!`;
+//     }
+
+//     [attacker, defender] = [defender, attacker];
+//   }
+// }
+
+// const krabby = new Pokemon("Krabby", 100, 50);
+// const squirtle = new Pokemon("Squirtle", 50, 0);
+
+// console.log(simulateBattle(krabby, squirtle, "Krabby"));
+
+// const pikachu = new Pokemon("Pikachu", 250, 4);
+// const charizard = new Pokemon("Charizard", 300, 5);
+
+// console.log(simulateBattle(pikachu, charizard, "Pikachu"));
+// console.log(simulateBattle(pikachu, charizard, "Charizard"));
+
+// Pokemon Constructor Function
 function Pokemon(name, health, attackBonus) {
   this.name = name;
   this.health = health;
   this.attackBonus = attackBonus;
 }
 
+// Adding methods to the Pokemon prototype
 Pokemon.prototype.biteAttack = function () {
   return this.attackBonus + 2;
 };
@@ -13,28 +55,26 @@ Pokemon.prototype.isDefeated = function () {
   return this.health <= 0;
 };
 
-function simulateBattle(pokemon1, pokemon2, firstAttackerName) {
-  let attacker = pokemon1.name === firstAttackerName ? pokemon1 : pokemon2;
-  let defender = pokemon1.name === firstAttackerName ? pokemon2 : pokemon1;
+// simulateBattle Function
+function simulateBattle(pokemon1, pokemon2, firstAttacker) {
+  let attacker = firstAttacker === pokemon1.name ? pokemon1 : pokemon2;
+  let defender = firstAttacker === pokemon1.name ? pokemon2 : pokemon1;
 
-  while (true) {
+  while (!pokemon1.isDefeated() && !pokemon2.isDefeated()) {
     defender.health -= attacker.biteAttack();
+    if (defender.isDefeated()) break;
 
-    if (defender.isDefeated()) {
-      return `${attacker.name} Wins!`;
-    }
-
+    // Swap attacker and defender
     [attacker, defender] = [defender, attacker];
   }
+
+  return pokemon1.isDefeated()
+    ? `${pokemon2.name} Wins!`
+    : `${pokemon1.name} Wins!`;
 }
 
-const krabby = new Pokemon("Krabby", 100, 50);
-const squirtle = new Pokemon("Squirtle", 50, 0);
+const pikachu = new Pokemon("Pikachu", 500, 4);
+const charizard = new Pokemon("Charizard", 300, 4);
 
-console.log(simulateBattle(krabby, squirtle, "Krabby"));
-
-const pikachu = new Pokemon("Pikachu", 250, 4);
-const charizard = new Pokemon("Charizard", 300, 5);
-
-console.log(simulateBattle(pikachu, charizard, "Pikachu"));
-console.log(simulateBattle(pikachu, charizard, "Charizard"));
+console.log(simulateBattle(pikachu, charizard, "Pikachu")); // Output: "Charizard Wins!"
+console.log(simulateBattle(charizard, pikachu, "charizard")); // Output: "Charizard Wins!"
