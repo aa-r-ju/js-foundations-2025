a7b8cc3c776ceb8a8a8de6f4710338d5521d67b3
aa-r-ju <aarju3447@gmail.com>
2025-01-30 19:44:48 +0545

    practicing the code

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,18 +1,88 @@
 /* eslint-disable no-unused-vars */
 // Function for `concatString`
+// function concatString() {
+//   return Array.from(arguments).join("");
+// }
+
+// function yourFunctionRunner() {
+//   return Array.from(arguments)
+//     .map((fn) => fn())
+//     .join("");
+// }
+
+// function makeAdder(x) {
+//   return function (y) {
+//     return x + y;
+//   };
+// }
+
+// function once(func) {
+//   let called = false;
+//   let result;
+//   return function () {
+//     if (!called) {
+//       called = true;
+//       result = func();
+//       return result;
+//     }
+//     return "the function has already been called...";
+//   };
+// }
+
+// function createObjectWithClosures() {
+//   let value = 0;
+//   return {
+//     oneIncrementer: function () {
+//       value += 1;
+//     },
+//     tensIncrementer: function () {
+//       value += 10;
+//     },
+//     getValue: function () {
+//       return value;
+//     },
+//     setValue: function (newValue) {
+//       value = newValue;
+//     },
+//   };
+// }
+
+// function dontSpillTheBeans(secret) {
+//   return {
+//     getSecret: function () {
+//       return secret;
+//     },
+//     setSecret: function (newSecret) {
+//       secret = newSecret;
+//     },
+//   };
+// }
+
 function concatString() {
   return Array.from(arguments).join("");
 }
 
+// console.log(concatString("This", " ", "should", " ", "be done with join"));
+
+const callThisFunction = () => {
+  return "Called Value";
+};
+
+const andThisFunction = () => {
+  return " and Other Value";
+};
+
 function yourFunctionRunner() {
   return Array.from(arguments)
-    .map((fn) => fn())
+    .map((val) => val())
     .join("");
 }
 
-function makeAdder(x) {
+// console.log(yourFunctionRunner(callThisFunction, andThisFunction));
+
+function makeAdder(int) {
   return function (y) {
-    return x + y;
+    return int + y;
   };
 }
 
@@ -33,16 +103,16 @@ function createObjectWithClosures() {
   let value = 0;
   return {
     oneIncrementer: function () {
-      value += 1;
+      return (value += 1);
     },
     tensIncrementer: function () {
-      value += 10;
+      return (value += 10);
     },
     getValue: function () {
       return value;
     },
-    setValue: function (newValue) {
-      value = newValue;
+    setValue: function (newVal) {
+      return (value = newVal);
     },
   };
 }
