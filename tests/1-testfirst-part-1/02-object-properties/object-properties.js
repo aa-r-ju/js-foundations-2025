85573488c6d2e0902f72bdfee3295b9539edc6f8
aa-r-ju <aarju3447@gmail.com>
2025-05-09 14:19:51 +0545

    feat: completed object-properties.js

--- tests/1-testfirst-part-1/02-object-properties/object-properties.js
+++ tests/1-testfirst-part-1/02-object-properties/object-properties.js
@@ -1,1 +1,34 @@
 /* eslint-disable no-unused-vars */
+function setPropsOnObj(obj) {
+  obj.x = 7;
+  obj["y"] = 8;
+  obj["onePlus"] = function (num) {
+    return num + 1;
+  };
+}
+
+function setPropsOnArr(arr) {
+  arr.hello = function () {
+    return "Hello!";
+  };
+  arr["full"] = "stack";
+  arr[0] = 5;
+  arr.twoTimes = function (num) {
+    return num * 2;
+  };
+}
+
+function setPropsOnFunc(val) {
+  val.year = "20??";
+  val.divideByTwo = function (num) {
+    return num / 2;
+  };
+}
+
+function shallowCopy(vgumBrandsal1, mintBrands) {
+  if (Array.isArray(vgumBrandsal1) && Array.isArray(mintBrands)) {
+    return [...vgumBrandsal1, ...mintBrands];
+  } else {
+    return { ...vgumBrandsal1, ...mintBrands };
+  }
+}
