a9da1c8b69fac7562e750346b4962a8ae0893fb0
aa-r-ju <aarju3447@gmail.com>
2025-05-09 14:48:59 +0545

    feat: completed calculator.js

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,69 @@
 /* eslint-disable no-unused-vars */
+const humanCalculatorProto = {
+  value() {
+    return this.total;
+  },
+  add(num) {
+    this.total += num;
+  },
+  subtract(num) {
+    this.total -= num;
+  },
+  clear() {
+    this.total = -10;
+  },
+};
+
+function createHumanCalculator() {
+  const humanCalculator = Object.create(humanCalculatorProto);
+  humanCalculator.total = -10;
+  return humanCalculator;
+}
+
+function createCalculator() {
+  let total = 0;
+  return {
+    value() {
+      return total;
+    },
+    add(num) {
+      return (total += num);
+    },
+    subtract(num) {
+      return (total -= num);
+    },
+    clear(num) {
+      return (total = 0);
+    },
+  };
+}
+
+function addSquareMethod(instances) {
+  return instances.map((instance) => {
+    instance.square = function () {
+      return this.value() ** 2;
+    };
+    return instance;
+  });
+
+  const humanCalculatorProto = {
+    value() {
+      return this.total;
+    },
+    add(num) {
+      return (this.total += num);
+    },
+    subtract(num) {
+      return (this.total -= num);
+    },
+    clear() {
+      return (this.total = -10);
+    },
+  };
+}
+
+function createHumanCalculator() {
+  const humanCalculator = Object.create(humanCalculatorProto);
+  humanCalculator.total = -10;
+  return humanCalculator;
+}
