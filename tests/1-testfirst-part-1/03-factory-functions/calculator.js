b252d49c19e05107accfc2ff4eb103ee8aaf3c98
aa-r-ju <aarju3447@gmail.com>
2025-05-09 14:56:22 +0545

    fix: done with more easy-way

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,4 +1,42 @@
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
+
+// ===============================
+// Basic Calculator using Factory
+// ===============================
+function createCalculator() {
+  let total = 0;
+
+  return {
+    value() {
+      return total;
+    },
+    add(num) {
+      total += num;
+    },
+    subtract(num) {
+      total -= num;
+    },
+    clear() {
+      total = 0;
+    },
+  };
+}
+
+// ====================================
+// Add square method to calculator list
+// ====================================
+function addSquareMethod(instances) {
+  return instances.map((instance) => {
+    instance.square = function () {
+      return this.value() ** 2;
+    };
+    return instance;
+  });
+}
+
+// ======================================
+// Human Calculator using Object.create()
+// ======================================
 const humanCalculatorProto = {
   value() {
     return this.total;
@@ -13,54 +51,6 @@ const humanCalculatorProto = {
     this.total = -10;
   },
 };
-
-function createHumanCalculator() {
-  const humanCalculator = Object.create(humanCalculatorProto);
-  humanCalculator.total = -10;
-  return humanCalculator;
-}
-
-function createCalculator() {
-  let total = 0;
-  return {
-    value() {
-      return total;
-    },
-    add(num) {
-      return (total += num);
-    },
-    subtract(num) {
-      return (total -= num);
-    },
-    clear(num) {
-      return (total = 0);
-    },
-  };
-}
-
-function addSquareMethod(instances) {
-  return instances.map((instance) => {
-    instance.square = function () {
-      return this.value() ** 2;
-    };
-    return instance;
-  });
-
-  const humanCalculatorProto = {
-    value() {
-      return this.total;
-    },
-    add(num) {
-      return (this.total += num);
-    },
-    subtract(num) {
-      return (this.total -= num);
-    },
-    clear() {
-      return (this.total = -10);
-    },
-  };
-}
 
 function createHumanCalculator() {
   const humanCalculator = Object.create(humanCalculatorProto);
