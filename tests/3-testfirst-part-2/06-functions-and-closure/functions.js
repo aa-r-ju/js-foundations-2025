7cbf17dfc1d4c77173d65b77fa9a48598a9e735a
aa-r-ju <aarju3447@gmail.com>
2025-05-14 21:47:47 +0545

    feat: completed concatString and yourFucntionRunner

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function concatString() {
+  return Array.from(arguments).join("");
+}
+console.log(concatString("This", " ", "should", " ", "be done with join"));
+
+function yourFunctionRunner() {
+  return Array.from(arguments)
+    .map((fun) => fun())
+    .join("");
+}
