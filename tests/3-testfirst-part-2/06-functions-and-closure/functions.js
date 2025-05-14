c867116c6763385714781b947799d5bd5176f30e
aa-r-ju <aarju3447@gmail.com>
2025-05-14 23:39:23 +0545

    feat: completed makeAdder,once,createObjectWithClosures,dontSpillTheBeans fucntions

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -9,3 +9,54 @@ function yourFunctionRunner() {
     .map((fun) => fun())
     .join("");
 }
+
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+function once(fun) {
+  let called = false;
+  let result;
+
+  return function () {
+    if (!called) {
+      called = true;
+      result = fun();
+      return result;
+    } else {
+      return "the function has already been called...";
+    }
+  };
+}
+
+function createObjectWithClosures() {
+  let value = 0;
+
+  return {
+    oneIncrementer: function () {
+      value += 1;
+    },
+    tensIncrementer: function () {
+      value += 10;
+    },
+    getValue: function () {
+      return value;
+    },
+    setValue: function (val) {
+      value = val;
+    },
+  };
+}
+
+function dontSpillTheBeans(secret) {
+  return {
+    getSecret: function () {
+      return secret;
+    },
+    setSecret: function (newSecret) {
+      secret = newSecret;
+    },
+  };
+}
