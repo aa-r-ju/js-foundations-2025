1c161e4ab2a22c078fbf12eea8eb700883bc1539
aa-r-ju <aarju3447@gmail.com>
2025-05-17 20:53:39 +0545

    feat: completed capitalize and swapCase functions

--- tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -2,3 +2,14 @@
 function indexAndValue(arr) {
   return arr.map((value, index) => ({ index, value }));
 }
+
+function capitalize(val) {
+  return val.toUpperCase();
+}
+
+function swapCase(str) {
+  return str
+    .split(" ")
+    .map((word, index) => (index % 2 === 0 ? capitalize(word) : word))
+    .join(" ");
+}
