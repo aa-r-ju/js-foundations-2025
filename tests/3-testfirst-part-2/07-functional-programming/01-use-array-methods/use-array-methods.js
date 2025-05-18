b9e3f35e201856bc5b8b8d6bae5033b77787e2cd
aa-r-ju <aarju3447@gmail.com>
2025-05-18 16:43:29 +0545

    feat: completed getPopulation function

--- tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -17,3 +17,12 @@ function swapCase(str) {
 function extensionSearch(extension, files) {
   return files.filter((file) => file.endsWith(`.${extension}`));
 }
+
+function getPopulation(countries, names) {
+  return countries.reduce((sum, country) => {
+    if (names.length === 0 || names.includes(country.name)) {
+      return sum + country.population;
+    }
+    return sum;
+  }, 0);
+}
