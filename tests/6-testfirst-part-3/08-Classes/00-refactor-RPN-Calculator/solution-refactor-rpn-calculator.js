00c5b6f437bbaa8ff44521881a40b7e400579d8f
aa-r-ju <aarju3447@gmail.com>
2025-06-03 14:49:15 +0545

    feat: completed solution-refactor-rpn-calculator

--- tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,40 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(num) {
+    this.stack.push(num);
+  }
+
+  popCalculator(func) {
+    if (this.stack.length < 2) {
+      throw "rpnCalculatorInstance is empty";
+    } else {
+      const num1 = this.stack.pop();
+      const num2 = this.stack.pop();
+
+      this.push(func(num1, num2));
+    }
+  }
+
+  plus() {
+    this.popCalculator((one, two) => two + one);
+  }
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  minus() {
+    this.popCalculator((one, two) => two - one);
+  }
+  times() {
+    this.popCalculator((one, two) => two * one);
+  }
+  divide() {
+    this.popCalculator((one, two) => two / one);
+  }
+}
